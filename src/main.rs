99342c6df8fa4c210877cdd090dbe5a4598d3823
Mateus Nunes <mateusnss@protonmail.ch>
2021-08-14 21:24:32 -0300

    Software architecture was created

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 use std::env;
 
 
+fn handle_commands() {
+    let mut arguments: Vec<String> = Vec::new();
 
-fn main() {
 
     for argument in env::args() {
-        println!("{}", argument);
+        arguments.push(String::from(argument));
+    }arguments.remove(0);
+
+
+    if arguments[0] == "new-key"{
+        generate_key(arguments[1].parse::<u32>().unwrap());
+        
+    }else if arguments[0] == "code"{
+        println!("code");
+    }else if arguments[0] == "decode"{
+        println!("decode");
     }
 
+    println!("ARGUMENTS::: {:?}", arguments);
+}
+
+
+fn generate_key(qnt:u32) {
+    println!("{}", qnt);
+}
+
+fn code() {}
+
+fn decode() {}
 
 
 
+
+fn main() {
+    handle_commands();
 }
