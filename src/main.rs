1d338c5c67a09cc0b83c72d1a6a60bff8f4b7210
Mateus Nunes <mateusnss@protonmail.ch>
2021-08-15 10:50:23 -0300

    Algorithm implemented for creating random sequence

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use rand::Rng;
 
 
 fn handle_commands() {
@@ -23,14 +24,37 @@ fn handle_commands() {
 }
 
 
+fn range(qnt:u32) -> u32 {
+    rand::thread_rng().gen_range(std::ops::Range{start: 0, end: qnt})
+}
+
+
 fn generate_key(qnt:u32) {
-    println!("{}", qnt);
+    let mut key_vec:Vec<u32> = Vec::new();
+    let mut n_for_comparation;
+    let mut repeat:bool = true;
+
+    let mut n:u32 = range(qnt);
+    key_vec.push(n);
+    
+    for _i in (std::ops::Range{start: 0, end: qnt - 1}){    
+        while repeat{
+            n = range(qnt);
+            n_for_comparation = n;
+            if key_vec.iter().filter(|&n| *n == n_for_comparation).count() == 0{
+                key_vec.push(n);
+                repeat = false;
+            }else{
+                repeat = true;
+            }
+        }repeat = true;
+    }
+    println!("{:?}", key_vec);
 }
 
 fn code() {}
 
 fn decode() {}
-
 
 
 
