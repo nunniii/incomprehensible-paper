af7f0c8d80348ab76a082a59029171ff6c82321f
Mateus Nunes <mateusnss@protonmail.ch>
2021-08-22 11:47:31 -0300

    organized

--- src/main.rs
+++ src/main.rs
@@ -7,88 +7,46 @@ fn range(qnt:u32) -> u32 {
 }
 
 
-fn handle_commands() {
-    let mut arguments: Vec<String> = Vec::new();
-    for argument in env::args() {
-        arguments.push(String::from(argument));
-    }arguments.remove(0);
 
 
-    if arguments[0] == "new-key"{
-
-        let key:Key = Key{components:generate_key(arguments[1].parse::<u32>().unwrap())};
-        println!(";;\t{}", key.format_key());
-
-        struct Key{
-            components:Vec<u32>
-        }
-
-        impl Key{
-            fn format_key(&self) -> String {
-        
-                let mut string_base:String = String::from("");
-                let mut aux;
-        
-                enum Last{SingleDigit,  MultipleDigit}
-                let mut last_number = Last::MultipleDigit;
-        
-                for i in &self.components{
-                    if i < &10{
-                        aux = format!("{}", i);   
-                        last_number = Last::SingleDigit;
-                    }else{
-                        match last_number{
-                            Last::SingleDigit => aux = format!(".{}.", i),
-                            Last::MultipleDigit => aux = format!("{}.", i)
-                        }last_number = Last::MultipleDigit;
-                    }string_base = string_base + String::as_str(&aux);
-                }
-                let final_formatting = &string_base.split_at(string_base.len() - 1);
-                if final_formatting.1 == "."{
-                    return String::from(final_formatting.0)
-                }else{
-                    return string_base
-                }
-            }
-        }
-
-
-
-
-
-
-
-
-    }else if arguments[0] == "code"{
-
-        let mut words:Vec<String> = Vec::new();
-        let mut key:String = String::from("");
-        let mut received_words:bool = false;
-        for i in arguments{
-            if i != "-k" && !received_words{
-                words.push(i);
-            }else if i == "-k"{
-                received_words = true;
-            }else{
-                key = String::from(&i);
-            }
-        }words.remove(0);
-
-        println!("dev - {{seed: {:?}}}", words);
-        println!("dev - {{key: {}}}", key);
-
-
-
-
-
-    }else if arguments[0] == "decode"{
-        println!("decode");
-    }
-
+struct Key{
+    components:Vec<u32>
 }
 
+impl Key{
+    fn format_key(&self) -> String {
 
+        let mut string_base:String = String::from("");
+        let mut aux;
 
+        enum Last{SingleDigit,  MultipleDigit}
+        let mut last_number = Last::MultipleDigit;
+
+        for i in &self.components{
+            if i < &10{
+                match last_number{
+                    Last::SingleDigit => aux = format!("{}", i),
+                    Last::MultipleDigit => aux = format!(".{}", i)
+                }
+                // aux = format!("{}.", i);   
+                last_number = Last::SingleDigit;
+            }else{
+                match last_number{
+                    Last::SingleDigit => aux = format!("-{}", i),
+                    Last::MultipleDigit => aux = format!("-{}", i)
+                }last_number = Last::MultipleDigit;
+            }string_base = string_base + String::as_str(&aux);
+        }
+
+        
+        let final_formatting = &string_base.split_at(string_base.len() - 1);
+        if final_formatting.1 == "."{
+            return String::from(final_formatting.0)
+        }else{
+            return string_base
+        }
+    }
+}
 
 fn generate_key(qnt:u32) -> Vec<u32> {
     let mut key_vec:Vec<u32> = Vec::new();
@@ -112,6 +70,71 @@ fn generate_key(qnt:u32) -> Vec<u32> {
     }
     return key_vec;
 }
+
+
+
+
+
+
+
+// struct Data{
+//     seed:Vec<Sring>,
+//     key:Option<String>
+// }
+
+
+
+
+fn handle_commands() {
+    let mut arguments: Vec<String> = Vec::new();
+    for argument in env::args() {
+        arguments.push(String::from(argument));
+    }arguments.remove(0);
+
+
+    if arguments[0] == "new-key"{
+
+        let key:Key = Key{components:generate_key(arguments[1].parse::<u32>().unwrap())};
+        println!(";;\t{}", key.format_key());
+
+
+
+
+
+    }else if arguments[0] == "code"{
+
+
+        
+
+        let mut words:Vec<String> = Vec::new();
+        let mut key:String = String::from("");
+        let mut received_words:bool = false; // receive true if all words have been passed to words:<Vec>
+
+        for i in arguments{
+            if i != "-k" && !received_words{
+                words.push(i);
+            }else if i == "-k"{
+                received_words = true;
+            }else{
+                key = String::from(&i);
+            }
+        }words.remove(0);
+
+        println!("dev - {{seed: {:?}}}", words);
+        println!("dev - {{key: {:?}}}", key);
+
+
+
+
+
+    }else if arguments[0] == "decode"{
+        println!("decode");
+    }
+
+}
+
+
+
 
 // fn code() {}
 
