1c14c4fe06310531927176edfc282cc221d9c6c1
Mateus Nunes <mateusnss@protonmail.ch>
2021-08-30 16:23:51 -0300

    Organized

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,34 @@ fn range(qnt:u32) -> u32 {
     rand::thread_rng().gen_range(std::ops::Range{start: 0, end: qnt})
 }
 
+fn generate_key(qnt:u32) -> Vec<u32> {
+    let mut key_vec:Vec<u32> = Vec::new();
+    let mut n_for_comparation;
+    let mut repeat:bool = true;
+
+    let mut n:u32 = range(qnt);
+    key_vec.push(n);
+    
+    for _i in (std::ops::Range{start: 0, end: qnt - 1}){    
+        while repeat{
+            n = range(qnt);
+            n_for_comparation = n;
+            if key_vec.iter().filter(|&n| *n == n_for_comparation).count() == 0{
+                key_vec.push(n);
+                repeat = false;
+            }else{
+                repeat = true;
+            }
+        }repeat = true;
+    }
+    return key_vec;
+}
 
 
+
+
+
+// Structs
 
 struct Key{
     components:Vec<u32>
@@ -48,35 +74,47 @@ impl Key{
     }
 }
 
-fn generate_key(qnt:u32) -> Vec<u32> {
-    let mut key_vec:Vec<u32> = Vec::new();
-    let mut n_for_comparation;
-    let mut repeat:bool = true;
 
-    let mut n:u32 = range(qnt);
-    key_vec.push(n);
-    
-    for _i in (std::ops::Range{start: 0, end: qnt - 1}){    
-        while repeat{
-            n = range(qnt);
-            n_for_comparation = n;
-            if key_vec.iter().filter(|&n| *n == n_for_comparation).count() == 0{
-                key_vec.push(n);
-                repeat = false;
-            }else{
-                repeat = true;
-            }
-        }repeat = true;
-    }
-    return key_vec;
+
+
+struct Data{
+    seed:Vec<String>,
+    key:String
+}
+
+fn receive_data(arguments:Vec<String>) -> Data {
+
+    let mut words:Vec<String> = Vec::new();
+    let mut key:String = String::from("");
+    let mut received_words:bool = false; // receive true if all words has been passed to words:<Vec>
+    let mut is_there_key:bool = false;
+    let data:Data;
+
+    for i in arguments{
+        if i != "-k" && !received_words{
+            words.push(i);
+        }else if i == "-k"{
+            received_words = true;
+            is_there_key = true;
+        }else{
+            key = String::from(&i);
+        }
+    }words.remove(0);
+
+    data = Data{
+        seed:words,
+        key:key
+    };
+    data
 }
 
 
 
+// fn key_reader() {}
 
+// fn code(seed:Vec<String>) -> ? {}
 
-
-
+// fn decode(seed:Vec<String>, key:Vec<u32>) -> seed:Vec<String> {}
 
 
 
@@ -92,6 +130,7 @@ fn handle_commands() {
     if arguments[0] == "new-key"{
 
         let key:Key = Key{components:generate_key(arguments[1].parse::<u32>().unwrap())};
+
         println!(";;\t{}", key.format_key());
 
 
@@ -101,44 +140,12 @@ fn handle_commands() {
     }else if arguments[0] == "code"{
 
 
-        struct Data{
-            seed:Vec<String>,
-            key:Option<String>
-        }
-
-        fn receive_data(arguments:Vec<String>) -> Data {
-
-            let mut words:Vec<String> = Vec::new();
-            let mut key:String = String::from("");
-            let mut received_words:bool = false; // receive true if all words has been passed to words:<Vec>
-            let mut is_there_key:bool = false;
-            let data:Data;
-
-            for i in arguments{
-                if i != "-k" && !received_words{
-                    words.push(i);
-                }else if i == "-k"{
-                    received_words = true;
-                    is_there_key = true;
-                }else{
-                    key = String::from(&i);
-                }
-            }words.remove(0);
-
-            data = Data{
-                seed:words,
-                key:Some(key)
-            };
-            data
-        }
-
-        
-        let i = receive_data(arguments);
+        let data = receive_data(arguments);
         
         
-        println!("dev - {{seed: {:?}}}", i.seed);
-        println!("dev - {{key: {:?}}}", i.key);
-
+        println!("dev - {{seed: {:?}}}", data.seed);
+        println!("dev - {{key: {:?}}}", data.key);
+        
 
 
 
@@ -151,10 +158,6 @@ fn handle_commands() {
 
 
 
-
-// fn code() {}
-
-// fn decode() {}
 
 
 
